7d6c8e418516245eca9d6231496bc39f8a9b7cb9
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-09-19 21:44:00 +0500

    Variable Declaration

---
+++ 3-JAVASCRIPT/Class_2/class_2.js
@@ -0,0 +1,21 @@
+//  Variables
+let a = "fhdjsahjf";
+a = 5;
+const c = null;
+
+
+
+// c = 15;
+// const c;
+
+// a = 20; // Reassigning 'a' is allowed
+// b = 25; // Reassigning 'b' is allowed
+// c = 30; // Reassigning 'c' will throw an error
+// console.log(b); // 25
+
+// function testScope() {
+//   console.log(a); // 5
+//   console.log(b);
+//   console.log(c); // 15
+// }
+// testScope();
