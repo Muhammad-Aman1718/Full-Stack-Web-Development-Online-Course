d68670056dc7442a3517d59736a7d8755de9ac37
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-09-21 21:40:09 +0500

    Input/Output

---
+++ 3-JAVASCRIPT/Class_4/class_4.js
@@ -0,0 +1,21 @@
+// let num = 10;
+// a = 89;
+
+// fdsajlk = 3;
+// memory address of num is 0x001
+// console.log(num);
+
+// table = {
+//   name: 0x001,
+//   a: 0x002,
+//   fdsajlk: 0x003,
+// };
+
+// let num = 10;
+// let num2 = num;
+
+let input = prompt("Enter a number : ");
+let num = +prompt("Enter a number 2 : ");
+console.log(input);
+console.log(num);
+document.write(input + num);
