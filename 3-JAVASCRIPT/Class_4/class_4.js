9ca3ec5b4df997e23d5d42daf2cab29c04e99504
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-09-24 21:33:48 +0500

    Input/Output

--- 3-JAVASCRIPT/Class_4/class_4.js
+++ 3-JAVASCRIPT/Class_4/class_4.js
@@ -14,8 +14,27 @@
 // let num = 10;
 // let num2 = num;
 
-let input = prompt("Enter a number : ");
-let num = +prompt("Enter a number 2 : ");
-console.log(input);
-console.log(num);
-document.write(input + num);
+// let input = prompt("Enter a name : ");
+// let num = +prompt("Enter a number 2 : ");
+// console.log(typeof input);
+// console.log(typeof num);
+// document.write(num);
+// alert(" this is number " + num);
+
+// let a = 10;
+// let b = 20;
+// c = "453";
+// fdsa = [];
+// console.log(typeof a);
+// console.log(String(a));
+// console.log(typeof String(a));
+// console.log(b);
+// console.log(c);
+// console.log(Number(c));
+// console.log(Boolean(c));
+// console.log(Boolean(fdsa));
+
+console.log(2 + "3");
+console.log(2 - "3");
+console.log(2 * "3");
+console.log(2 / "3");
