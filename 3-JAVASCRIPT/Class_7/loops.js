d5abce7cb2d43aac66560a4bbe69c26d4253d017
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-09-27 21:29:02 +0500

    Loops

---
+++ 3-JAVASCRIPT/Class_7/loops.js
@@ -0,0 +1,20 @@
+// Loops in JavaScript
+
+// For Loop
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(arr.length);
+dex = 0; index < 100; index++) {
+  console.log(index);
+}
+for (let in
+
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+
+// let a = 0; // a = a+1
+// a += 1;
+// console.log(a);
