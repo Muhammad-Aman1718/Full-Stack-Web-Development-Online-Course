eb03c8d177757dde71e9a376e419587995cae44c
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-03 21:38:24 +0500

    String

---
+++ 3-JAVASCRIPT/Class_9/string.js
@@ -0,0 +1,61 @@
+// String Manipulation in JavaScript
+
+// let str = "Hello, World!";
+// console.log("Original String:", str);
+
+// Slicing
+// console.log(str.slice(0, 5)); // "Hello"
+// console.log(str.slice(5)); // "Hello"
+// console.log(str.slice(0, 5)); // "Hello"
+
+// let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let length = text.length;
+// console.log("Length of text:", length); // 26
+
+// charAt()
+// console.log(str.charAt(0));
+
+// const name = "W3Schools";
+// // let letter = name.at(2);
+// // console.log(name.at(2)); // "3"
+
+// // Trim
+
+// let str1 = "       Hello World!        ";
+// console.log(str1.trim()); // "Hello World!"
+// console.log(str1.trimStart()); // "Hello World!        "
+// console.log(str1.trimEnd()); // "       Hello World!"
+
+// //split()
+
+// let txt = "a b c,d,e,f";
+// console.log(txt.split(" ")); // ["a", "b", "c", "d", "e", "f"]
+
+// replace()
+// let text1 = "Please visit Microsoft and Microsoft";
+// console.log(text1.replaceAll("Microsoft", "W3Schools")); // "Please visit W3Schools and Microsoft"
+
+// // repeat()
+// let str2 = "Hello ";
+// console.log(str2.repeat(3)); // "Hello Hello Hello "
+
+// toLowerCase() and toUpperCase()
+// let text2 = "Hello World!";
+// console.log(text2.toLowerCase());
+// console.log(text2.toUpperCase());
+
+// concat()
+// let text1 = "Hello";
+// let text2 = "World";
+// console.log(text1.concat(" ", text2)); // "Hello World"
+// console.log(text1 + " " + text2); // "Hello World"
+
+// Built-in Functions
+
+Number();
+string();
+Boolean();
+parseInt();
+parseFloat();
+
+// .built-method
