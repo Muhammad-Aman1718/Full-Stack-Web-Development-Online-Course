66813f880ae62c979a0dd69e068bc9ee0745f071
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-06 23:27:54 +0500

    Online-Test

---
+++ Online-Test/array_and_strings_methods.js
@@ -0,0 +1,95 @@
+// 🧵 Array Methods
+
+// Q3.
+// Given:
+// const prices = [10, 20, 30, 40];
+// Find the total sum of all elements using a built-in method.
+
+// Q4.
+// Given:
+// const fruits = ["apple", "banana", "mango", "kiwi"];
+// Return the first fruit whose length is greater than 5.
+
+// Q5.
+// Given:
+// const nums = [1, 2, 3, 4, 5];
+// Return a new array containing only the 2nd and 3rd elements.
+
+// Q6.
+// Given:
+// const arr = [5, 10, 15, 20];
+// Find the index of the value 15.
+
+// Q7.
+// Given:
+// const words = ["hello", "world", "javascript"];
+// Combine them into a single string separated by a hyphen (-).
+
+// Q8.
+// Given:
+// const data = [1, 2, 3, 4, 5];
+// Reverse the order of the array.
+
+// Q9.
+// Given:
+// const arr = ["apple", "banana"];
+// Add "mango" at the end and "orange" at the beginning.
+
+// Q10.
+// Given:
+// const arr = [2, 4, 6];
+// Check:
+// If any number is greater than 5.
+// If all numbers are even.
+
+// 🧵 String Methods
+
+// Q11.
+// Given:
+// const str = "JavaScript is fun";
+// Convert the string to both uppercase and lowercase.
+
+// Q12.
+// Given:
+// const str = "Hello World";
+// Check if "World" exists in the string.
+
+// Q13.
+// Given:
+// const str = "apple,banana,orange";
+// Convert this string into an array of fruits.
+
+// Q14.
+// Given:
+// const str = "   hello js   ";
+// Remove extra spaces from the start and end.
+
+// Q15.
+// Given:
+// const str = "I love coding";
+// Replace the word "coding" with "JavaScript".
+
+// Q16.
+// Given:
+// const str = "Frontend Developer";
+// Extract only the word "Frontend".
+
+// Q17.
+// Given:
+// const str = "abc";
+// Repeat this string three times.
+
+// Q18.
+// Given:
+// const str = "Learn JavaScript";
+// Check if it starts with "Learn" and ends with "Script".
+
+// Q19.
+// Given:
+// const str = "javascript";
+// Get the 4th character and the index of 's'.
+
+// Q20.
+// Given:
+// const sentence = "Coding is awesome";
+// Convert it into "Coding-is-awesome" using built-in methods.
