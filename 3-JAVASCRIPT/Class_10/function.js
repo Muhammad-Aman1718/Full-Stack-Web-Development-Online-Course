63d1c0cbf848f4023799a5e714f8ede9407b1b47
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-08 21:27:50 +0500

    Funtions

---
+++ 3-JAVASCRIPT/Class_10/function.js
@@ -0,0 +1,27 @@
+//  Functions in JavaScript
+// greet(null, null);
+// function greet(name, age) {
+//   console.log("this is my name  :" + name + " and my age is :" + age);
+// }
+
+// greet("Alice", "25");
+// greet("amna", "22");
+// greet("umer", "26");
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// // console.log(sum(5, 10));
+// // console.log(sum(20, 30));
+// // console.log(sum(100, 200));
+
+// value = sum(15, 25) * 2;
+// console.log(value);
+
+// array = [1, 2, 3];
+// array.push(4);
+
+// Arrow Function
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 3)); // Outputs: 6
