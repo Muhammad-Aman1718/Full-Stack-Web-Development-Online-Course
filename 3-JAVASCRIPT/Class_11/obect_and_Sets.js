f2cebc3be55b9a7de9604fa535af5a2788ca809f
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-23 21:42:47 +0500

    Objects and sets

--- 3-JAVASCRIPT/Class_11/obect_and_Sets.js
+++ 3-JAVASCRIPT/Class_11/obect_and_Sets.js
@@ -1,3 +1,44 @@
-// import str from "../Class_9/string.js";
+const sets = new Set();
 
-// console.log(str);
+// sets.add(12);
+// sets.add(123);
+// sets.add(14);
+// console.log(sets.has(12));
+// console.log(sets.has(143));
+// console.log(sets.size);
+// sets.delete(12);
+// sets.clear()
+// console.log(sets);
+
+// const object = {
+//   name: "Aman",
+//   age: 21,
+//   level: 43,
+//   a: [1, 32, 4, 65],
+//   fdsah: new Set(),
+//   sahfkj: "fd",
+// };
+
+// // object.fdsah.add(34);
+// // object.fdsah.add(3454);
+// // object.fdsah.add(34);
+// // console.log(object.fdsah);
+
+// console.log(object.a[0]);
+
+const object = {
+  name: "aman",
+  3548: 5467,
+  age: 32,
+  a: [1, 35, 5],
+  true: 7843,
+};
+for (let i = 0; i <= 10; i++) {
+  object[i] = i + 1;
+}
+
+object[name] = "afdsaman";
+object[223] = 7843;
+object[true] = 43;
+console.log(object);
+console.log("fdjsk");
